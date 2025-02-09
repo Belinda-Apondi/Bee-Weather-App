62e62b15bf4325606369f1fe67aa014e170c8e0f
LadyBee-stack <mboyaapondi@gmail.com>
2025-02-09 04:16:16 -0800

    html minor span changes

--- index.js
+++ index.js
@@ -53,4 +53,4 @@ function myCity(event, theCityValue = "Nairobi") {
 let myValue = document.querySelector("#city");
 myValue.addEventListener("submit", myCity);
 document.addEventListener("DOMContentLoaded", function () {
-  myCity(null, "Thika")});
+  myCity(null, "Nairobi")});
